130b461f5d17a2c8b7e2d04a309396daede474cd
Jianda LIN <jianda_lin@hotmail.fr>
2020-01-13 11:01:38 +0100

    Ajout classe cinema

---
+++ src/main/java/com/montparnasse/cinema/domaine/Cinema.java
@@ -0,0 +1,86 @@
+package com.montparnasse.cinema.domaine;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Cinema {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private double longitude;
+	private double latitude;
+	private double altitude;
+	private int nombreSalles;
+	
+	  
+	
+	/*------constructeur vide----*/
+	public Cinema() {
+		super();
+	}
+	
+	/*-----------constructeur-----*/
+
+	public Cinema(String name, double longitude, double latitude, double altitude, int nombreSalles) {
+		super();
+		this.name = name;
+		this.longitude = longitude;
+		this.latitude = latitude;
+		this.altitude = altitude;
+		this.nombreSalles = nombreSalles;
+	}
+
+	/*---------getter/setter---------------*/
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public double getAltitude() {
+		return altitude;
+	}
+	public void setAltitude(double altitude) {
+		this.altitude = altitude;
+	}
+	public int getNombreSalles() {
+		return nombreSalles;
+	}
+	public void setNombreSalles(int nombreSalles) {
+		this.nombreSalles = nombreSalles;
+	}
+
+	/*-------------------toString-----------*/
+	@Override
+	public String toString() {
+		return "Cinema [id=" + id + ", name=" + name + ", longitude=" + longitude + ", latitude=" + latitude
+				+ ", altitude=" + altitude + ", nombreSalles=" + nombreSalles + "]";
+	}
+	
+	
+	
+	
+} // fin classe
