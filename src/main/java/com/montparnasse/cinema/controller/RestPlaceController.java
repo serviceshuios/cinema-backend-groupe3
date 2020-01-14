d0fa2ed749ffd01c3dd9545a2617245965e5a19b
Corentin BOUET <corentinbouet@orange.fr>
2020-01-14 10:48:09 +0100

    Mise à jour cross Origin

--- src/main/java/com/montparnasse/cinema/controller/RestPlaceController.java
+++ src/main/java/com/montparnasse/cinema/controller/RestPlaceController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.montparnasse.cinema.domaine.Place;
 import com.montparnasse.cinema.service.IPlaceService;
 
+@CrossOrigin("*")
 @RestController
 public class RestPlaceController {
 
--- src/main/java/com/montparnasse/cinema/controller/RestProjectionFilmController.java
+++ src/main/java/com/montparnasse/cinema/controller/RestProjectionFilmController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +18,7 @@ import com.montparnasse.cinema.service.IFilmService;
 import com.montparnasse.cinema.service.IProjectionService;
 import com.montparnasse.cinema.service.ISalleService;
 
+@CrossOrigin("*")
 @RestController
 public class RestProjectionFilmController {
 
